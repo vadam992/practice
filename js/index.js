81e98f5b17bd6a2944141075afaa10be959d830c
Váczi Ádám <adam.vaczi@oander.hu>
2022-06-05 20:16:51 +0200

    add webpack

---
+++ js/index.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+
+document.addEventListener("DOMContentLoaded", function() { 
+  $('body').addClass('asdASD');
+  //document.querySelector('body').classList.add('asd');
+});
