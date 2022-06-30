92bd0fbbb89e01be1cb5a401200019bbcfc89174
Váczi Ádám <adam.vaczi@oander.hu>
2022-06-30 18:03:02 +0200

    fetch and cards

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function() {
+  //accordion js
   $('.accordion__row').each(function() {
     $(this).click(function() {
       var panel = this.nextElementSibling;
@@ -13,4 +14,23 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     })
   })
+
+  //fetch js
+  fetch('https://retoolapi.dev/StDACM/bodrogi')
+  .then((response) => {
+    return response.json();
+  })
+  .then((datas) => {
+    console.log(datas);
+    var cardElement = '';
+
+    datas.map(function(data) {
+      cardElement += '<a class="col-lg-3 card__box" href="'+ data.link +'">';
+      cardElement += '<div class="card__image"><img src="'+ data.image +'" /></div>';
+      cardElement += '<div class="card__info"><div class="card__title">'+ data.title +'</div><div class="card__category">'+ data.category +'</div><div class="card__button">'+ data.button_title +'</div></div>';
+      cardElement += '</a>';
+    })
+
+    $('.card__row').append(cardElement);
+  });
 });
